4cf11eb087e12a75a8398e45e12379b564f0268d
Zhiyao Yan <zhy038@UCSD.EDU>
2017-01-22 17:51:24 -0800

    create brick wall

---
+++ HW1/Assets/SpawnBricks.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBricks : MonoBehaviour {
+    [SerializeField] private GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start()");
+
+        //GameObject spawnedBrick;
+        //Quaternion startRotation = Quaternion.Euler(Vector3.zero);
+
+        /*for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i + 2.0f, 0, 0), Quaternion.Euler(new Vector3(0.0f, 45.0f, 0.0f)));
+        }*/
+        int numBricks = 17;
+        for (int i = 0; i < 10; i++)
+        {
+            float offset = i % 2 == 0 ? 0.0f : getRotation(1, numBricks) / 2.0f;
+            spawnBrickLayer(i, numBricks, offset);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Update()");
+	}
+
+    void spawnBrickLayer(int layer, int numBricks, float offset)
+    {
+        BoxCollider boxCollider = prefab.GetComponent<BoxCollider>();
+        Vector3 scale = prefab.transform.localScale;
+        float sizeY = boxCollider.size.y * scale.y;
+
+        float r = 2.9f;
+
+        for (int i = 0; i < numBricks; i++)
+        {
+            float theta = getRotation(i, numBricks) + offset;
+            float deg = radToDeg(theta);
+            Debug.Log(theta);
+            Instantiate(
+                prefab,
+                //new Vector3(i + 2.0f, layer * sizeY, 0),
+                new Vector3(r * Mathf.Cos(theta), layer * sizeY, r * Mathf.Sin(theta)),
+                Quaternion.Euler(new Vector3(0.0f, -deg + 90.0f, 0.0f))
+                );
+        }
+    }
+
+    float getRotation(int i, int n)
+    {
+        return (2.0f * Mathf.PI) / n * i;
+    }
+
+    float radToDeg(float rad)
+    {
+        return rad / (Mathf.PI * 2.0f) * 360.0f;
+    }
+}
