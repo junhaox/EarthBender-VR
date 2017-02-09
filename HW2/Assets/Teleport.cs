13fcb2562f00513c591fc9ada2b5b3bef9587edf
Jimmy Yan <yjimmyy@gmail.com>
2017-02-09 13:48:58 -0800

    Implement teleportation

---
+++ HW2/Assets/Teleport.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject rightHand;
+
+    [SerializeField]
+    private GameObject cube;
+
+    private bool aPressed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Debug.Log("A pressed");
+            teleport();
+        }
+	}
+
+    void teleport()
+    {
+        Ray ray = new Ray(rightHand.transform.position, rightHand.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag.Equals("Floor"))
+            {
+                transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            }
+        }
+    }
+}
