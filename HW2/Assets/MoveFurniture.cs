9bfc394fdea7ed5e89c4c6df1e5a4d8ddc2c28b4
Jimmy Yan <yjimmyy@gmail.com>
2017-02-09 18:06:52 -0800

    add furniture movement

---
+++ HW2/Assets/MoveFurniture.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFurniture : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject rightHand;
+
+    private GameObject selected = null;
+    private float dist = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        {
+            Debug.Log("R trigger pressed");
+            selectObject();
+        } else if (OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger))
+        {
+            Debug.Log("R trigger released");
+            releaseObject();
+        }
+
+        moveObject();
+    }
+
+    private void selectObject()
+    {
+        Ray ray = new Ray(rightHand.transform.position, rightHand.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag.Equals("Furniture") || hit.collider.tag.Equals("Whiteboard"))
+            {
+                selected = hit.collider.gameObject;
+                dist = hit.distance;
+                Debug.Log("Furniture selected");
+            }
+        }
+    }
+
+    private void releaseObject()
+    {
+        selected = null;
+        dist = 0;
+    }
+
+    private void moveObject()
+    {
+        if (selected != null)
+        {
+            if (selected.tag.Equals("Furniture"))
+            {
+                moveFurniture();
+            } else if (selected.tag.Equals("Whiteboard"))
+            {
+                Debug.Log("Move Whiteboard");
+                moveWhiteboard();
+            }
+        }
+    }
+
+    private void moveFurniture()
+    {
+        Transform touchTrans = rightHand.transform;
+        //selected.transform.rotation = rightHand.transform.rotation;
+        selected.transform.position = touchTrans.position + touchTrans.forward * dist;
+    }
+
+    private void moveWhiteboard()
+    {
+        Ray ray = new Ray(rightHand.transform.position, rightHand.transform.forward);
+        RaycastHit[] hits;
+
+        hits = Physics.RaycastAll(ray, Mathf.Infinity);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.tag.Equals("Wall"))
+            {
+                selected.transform.position = hit.point;
+                selected.transform.rotation = Quaternion.LookRotation(selected.transform.forward, -hit.normal);
+                break;
+            }
+        }
+    }
+}
