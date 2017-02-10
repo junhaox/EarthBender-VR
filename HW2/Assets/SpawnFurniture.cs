5533a9f0cc9c2ca1a70c2e0b90d7c87778f0d22b
Jimmy Yan <yjimmyy@gmail.com>
2017-02-09 16:13:24 -0800

    add furniture spawning

--- HW2/Assets/SpawnFurniture.cs
+++ HW2/Assets/SpawnFurniture.cs
@@ -10,6 +10,17 @@ public class SpawnFurniture : MonoBehaviour {
     [SerializeField]
     private GameObject chair;
 
+    [SerializeField]
+    private GameObject locker;
+
+    [SerializeField]
+    private GameObject cabinet;
+
+    [SerializeField]
+    private GameObject tv;
+
+    private int type = 0;
+
     // Use this for initialization
     void Start ()
     {
@@ -21,13 +32,37 @@ public class SpawnFurniture : MonoBehaviour {
         {
             Debug.Log("X pressed");
             spawnFurniture();
+        } else if (OVRInput.GetDown(OVRInput.RawButton.Y))
+        {
+            Debug.Log("Y pressed");
+            type = (type + 1) % 5;
         }
     }
 
     private void spawnFurniture()
     {
+        GameObject furniture;
+        switch (type)
+        {
+            case 0:
+                furniture = desk;
+                break;
+            case 1:
+                furniture = chair;
+                break;
+            case 2:
+                furniture = locker;
+                break;
+            case 3:
+                furniture = cabinet;
+                break;
+            case 4:
+            default:
+                furniture = tv;
+                break;
+        }
         Instantiate(
-                desk,
+                furniture,
                 new Vector3(0.0f, 20.0f, 0.0f),
                 Quaternion.Euler(new Vector3(0.0f, 1.0f, 0.0f))
                 );
