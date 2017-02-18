752cfb8742e1fe2cbbdd4a53018ddebc05400026
Junhao Xie <juxie@UCSD.EDU>
2017-02-17 16:23:43 -0800

    Add HW3

---
+++ HW3/Assets/engineering-campus-small/Load.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Load : MonoBehaviour {
+    public ArrayList points;
+	// Use this for initialization
+	void Start () {
+        points = new ArrayList();
+        LoadData();
+        DrawData();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadData()
+    {
+        string[] lines = System.IO.File.ReadAllLines("comptetion-track.txt");
+        foreach (string line in lines)
+        {
+            char[] delim = { ' ' };
+            string[] coordinates = line.Split(delim);
+
+            GameObject point = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            Vector3 pos = new Vector3(float.Parse(coordinates[0]), float.Parse(coordinates[1]), float.Parse(coordinates[2]));
+            point.transform.position = pos + transform.position;
+            points.Add(point);
+        }
+    }
+
+    void DrawData()
+    {
+        foreach (GameObject point in points)
+        {
+            point.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
