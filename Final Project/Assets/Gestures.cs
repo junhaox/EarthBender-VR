1d0fb18f22ab80fb7e077bbbaa5c4dc1302016ce
Jimmy Yan <yjimmyy@gmail.com>
2017-03-10 17:50:38 -0800

    Add rocks

---
+++ Final Project/Assets/Gestures.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gestures : MonoBehaviour {
+
+    public GameObject rock;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OpenPalmUp()
+    {
+        //Debug.Log("OpenPalmUp");
+        Vector3 position = new Vector3(25.0f, 4.0f, 25.0f);
+        Instantiate(rock, position, Quaternion.identity);
+    }
+
+    void Test()
+    {
+        Debug.Log("palm up");
+    }
+}
