9598a0a2bffa6e2100ab459466da8fac05fc6147
Jimmy Yan <yjimmyy@gmail.com>
2017-03-15 20:15:32 -0700

    add reticle

---
+++ Final Project/Assets/Reticle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour {
+
+    private static float RETICLE_SCALE = 40.0f;
+
+    private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+        transform.position = camera.transform.position + camera.transform.forward * 3.0f;
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Debug.Log("HIT " + hit.point);
+            Vector3 hitPos = hit.point;
+            //hitPos.y = 2.0f;
+            transform.position = hitPos - camera.transform.forward.normalized * 1.0f;
+            transform.forward = camera.transform.forward;
+            float dist = (transform.position - camera.transform.position).magnitude;
+            float scale = dist / RETICLE_SCALE;
+            transform.localScale = new Vector3(scale, scale);
+        } else
+        {
+            float dist = 50.0f;
+            transform.position = camera.transform.position + camera.transform.forward * dist;
+            transform.forward = camera.transform.forward;
+            float scale = dist / RETICLE_SCALE;
+            transform.localScale = new Vector3(scale, scale);
+        }
+    }
+}
